450852bcb39251a9a55cc20a154943d06a9583f3
anhtt2211 <trananh.2211.work@gmail.com>
2023-11-28 17:31:09 +0700

    update: config absolute path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
-import { DatabaseModule } from "./database/database.module";
-import { MediaModule } from "./media/media.module";
-import { RabbitMqModule } from "./rabbitmq/rabbitmq.module";
-import { RedisModule } from "./redis/redis.module";
-import { UserModule } from "./user/user.module";
+import { DatabaseModule } from "@database/database.module";
+import { MediaModule } from "@media/media.module";
+import { RabbitMqModule } from "@rabbitmq/rabbitmq.module";
+import { RedisModule } from "@redis/redis.module";
+import { UserModule } from "@user/user.module";
 
 @Module({
   imports: [
